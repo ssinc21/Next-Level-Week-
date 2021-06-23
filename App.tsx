2f82523b73b7a1a2f4f2afafb820ee41adb84571
Inácio Silva <inacio.silva.1999@gmail.com>
2021-06-23 10:54:40 -0300

    Iniciando Home Screen

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 
 import { SignIn } from './src/screens/SignIn';
-
+import { Background } from "./src/components/Background";
 
 
 export default function App() {
@@ -22,13 +22,16 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignIn />
-    </>
+
+    </Background>
+
+
   );
 }
