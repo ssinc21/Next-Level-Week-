08bbcef305a0ada6f3bed12a53458dbbd11c9cbe
Inácio Silva <inacio.silva.1999@gmail.com>
2021-06-25 23:01:20 -0300

    Final da Aula 02

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { FlatList, View, Text } from "react-native";
 import { styles } from "./styles";
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
 import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
 
 export function Home() {
     const [category, setCategory] = useState('');
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '25/06/2021 às 15:08h',
+            description: 'É hoje que vamos chegar ao Supremo sem perder partida'
+        }
+    ]
+
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId);
     }
@@ -29,6 +45,14 @@ export function Home() {
                     <ListHeader
                         title="Partidas Agendadas"
                         subtitle="Total 6"
+                    />
+
+                    <FlatList
+                        data={appointments}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <Appointment data = {item}/>
+                        )}
                     />
                 </View>
             </View>
