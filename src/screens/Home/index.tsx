b2bf6837038798333661ba99e5ed0e594b060391
Inácio Silva <inacio.silva.1999@gmail.com>
2021-06-27 18:58:19 -0300

    Inicio Aula 03 12min

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,62 +1,79 @@
-import React, { useState } from "react";
-import { FlatList, View, Text } from "react-native";
-import { styles } from "./styles";
-import { Profile } from "../../components/Profile";
-import { ButtonAdd } from "../../components/ButtonAdd";
-import { CategorySelect } from "../../components/CategorySelect";
-import { ListHeader } from "../../components/ListHeader";
-import { Appointment } from "../../components/Appointment";
+import React, { useState } from 'react';
+import { View, FlatList } from 'react-native';
+
+import { CategorySelect } from '../../components/CategorySelect';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
+
+import { styles } from './styles';
 
 export function Home() {
-    const [category, setCategory] = useState('');
-    const appointments = [
-        {
-            id: '1',
-            guild: {
-                id: '1',
-                name: 'Lendários',
-                icon: null,
-                owner: true
-            },
-            category: '1',
-            date: '25/06/2021 às 15:08h',
-            description: 'É hoje que vamos chegar ao Supremo sem perder partida'
-        }
-    ]
+  const [category, setCategory] = useState('');
 
-    function handleCategorySelect(categoryId: string) {
-        categoryId === category ? setCategory('') : setCategory(categoryId);
-    }
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'      
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'      
+    },
+    
+  ]
 
-    return (
-        <View>
-            <View style={styles.header}>
-                <Profile />
-                <ButtonAdd />
-            </View>
-            <View>
-                <CategorySelect
-                    categorySelected={category}
-                    setCategory={handleCategorySelect}
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }  
 
-                />
+  return (
+    <View>
+      <View style={styles.header}>
+        <Profile />
+        <ButtonAdd />
+      </View>
+    
+      <CategorySelect 
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
 
-                <View style={styles.content}>
-                    <ListHeader
-                        title="Partidas Agendadas"
-                        subtitle="Total 6"
-                    />
+      <View style={styles.content}>
+        <ListHeader 
+          title="Partidas agendadas"
+          subtitle="Total 6"
+        />
 
-                    <FlatList
-                        data={appointments}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) => (
-                            <Appointment data = {item}/>
-                        )}
-                    />
-                </View>
-            </View>
-        </View>
-
-    );
+        <FlatList 
+            data={appointments}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+            <Appointment data={item} />            
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );  
 }
